3758b318891e06747b62586c77f6561809439f51
Nickobellic <avrahulkanna17@gmail.com>
2025-03-27 16:07:07 +0530

    Update ith bit

--- bit_manip_2.cpp
+++ bit_manip_2.cpp
@@ -1,6 +1,24 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
+void update_ith_bit(int &num, int pos, int bitValue) {
+    int temp = ~(1 << pos);
+
+    int clearedBit = num & temp;
+
+    int setTemp = 0;
+    if(bitValue == 1) {
+        setTemp = 1 << pos;
+    } 
+    num = clearedBit | setTemp;
+}
+
 int main() {
-    
+    // 0 1 0 1 0 1 = 21
+    int num = 21, second = 21;
+    update_ith_bit(num,0,0);
+    cout << num << endl; // Should return 20
+
+    update_ith_bit(second, 1, 1); 
+    cout << second << endl; // should return 23
 }
