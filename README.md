584527aa602287af2bc1f41865e361a9a75f9d41
Nickobellic <avrahulkanna17@gmail.com>
2025-03-27 16:26:24 +0530

    Clear last i bits

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bit Manipulation
+
+## bit_manip_1.cpp
+ Get, Set, Clear ith Bit
+
+## bit_manip_2.cpp
+ Update ith Bit, Clear last i bits
--- bit_manip_2.cpp
+++ bit_manip_2.cpp
@@ -13,6 +13,12 @@ void update_ith_bit(int &num, int pos, int bitValue) {
     num = clearedBit | setTemp;
 }
 
+void clear_i_bits(int &num, int pos) {
+    int temp = -1 << pos;
+
+    num = num & temp;
+}
+
 int main() {
     // 0 1 0 1 0 1 = 21
     int num = 21, second = 21;
@@ -21,4 +27,9 @@ int main() {
 
     update_ith_bit(second, 1, 1); 
     cout << second << endl; // should return 23
+
+    // 1 1 1 0 1 0 1 =   117
+    int n = 117;
+    clear_i_bits(n, 3);
+    cout << n << endl; // should get 112
 }
