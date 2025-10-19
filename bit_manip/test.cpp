f435a09b926547174cc7534c5976d9877cde8a36
Nickobellic <avrahulkanna17@gmail.com>
2025-10-19 08:40:19 +0530

    Practise of Count Bits

---
+++ bit_manip/test.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+void clearIthBit(int &n, int pos) {
+    int mask = ~(1 << pos);
+    n = n&mask;
+}
+
+void setIthBit(int &n, int pos) {
+    int mask = 1<<pos;
+
+    n = n | mask;
+}
+
+void updateIthBit(int &n, int pos, int val) {
+    if(val == 0) {
+        int clearMask = ~(1 << pos);
+        n = n&clearMask;
+
+    } else if(val == 1) {
+        int setMask = 1 << pos;
+        n = n|setMask;
+    }
+}
+
+void clearLastIBits(int &n, int count) {
+    int mask = (~0)<<count;
+
+    n = n&mask;
+}
+
+void clearBitRanges(int &n, int i, int j){
+    int mask1 = (1<<(i)) - 1;
+    int mask2 = ~(1<<(j));
+
+    int full_mask = mask1 | mask2;
+
+    n = n & full_mask;
+
+}
+
+// Time Complexity: O(log N) 
+// Eg: 64: 2^6 (atmost 7 bits)
+int countBits(int num){
+    int count = 0;
+
+    while(num > 0) {
+        count += (num&1); // Taking the last bit (if 1, increment)
+        num = num>>1; // Leftshifting: 16->8->4->2
+    }
+
+    return count;
+}
+
+int main() {
+    
+
+    cout << countBits(14); // 4
+
+}
