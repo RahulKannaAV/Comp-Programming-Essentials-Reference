d24fd6b43417022ee2a6fca73049c2a730000777
Nickobellic <avrahulkanna17@gmail.com>
2026-01-08 17:18:50 +0530

    Big Integer Addition Corrections

--- big_integers/big_int_addition.cpp
+++ big_integers/big_int_addition.cpp
@@ -9,23 +9,24 @@ void big_int_addition(string num1, string num2) {
 
     for(int i=0; i<num2.length(); i++) {
         if(i >= num1.length()) {
-            int revIdx = num2.length() - i - 1;
+            int revIdx = num2.length() - i - 1; // Going in reverse order
+
             int digitAddition = num2[revIdx] + carry - 48;
-            int nextDigitInt = digitAddition % 10;
+            int nextDigitInt = digitAddition % 10; // Taking only the last digit to add in the result. Remaining to the carry
             carry = digitAddition/10;
 
             string nextDigit = to_string(nextDigitInt);
             result.append(nextDigit);
         } else {
-            int num2_revIdx = num2.length() - i - 1;
+            int num2_revIdx = num2.length() - i - 1; // Going in reverse order
             int num1_revIdx = num1.length() - i - 1;
 
-            int digitNum1 = num1[num1_revIdx] - 48;
+            int digitNum1 = num1[num1_revIdx] - 48; // ASCII Char to digit
             int digitNum2 = num2[num2_revIdx] - 48;
 
             int digitSums = digitNum1 + digitNum2 + carry;
-            int nextDigitInt = digitSums % 10;
-            carry = digitSums / 10;
+            int nextDigitInt = digitSums % 10; // Taking the last digit for result
+            carry = digitSums / 10; // Remaining as carry
 
             string nxtDigit = to_string(nextDigitInt);
             result.append(nxtDigit);
@@ -33,7 +34,11 @@ void big_int_addition(string num1, string num2) {
         }
     }
 
-    for(int j=result.length()-1; j>=0; j--) {
+    if(carry == 1) {
+        result.push_back('1'); // At the end, if there's a value in carry other than 0 (i.e 1), add it in the result
+    }
+
+    for(int j=result.length()-1; j>=0; j--) { // Since, the result will be in reverse order, printing it in our desired order
         cout << result[j];
     }
     cout << endl;
@@ -42,8 +47,8 @@ void big_int_addition(string num1, string num2) {
 int main() {
     // bigger number at num2
     // smaller number at num1
-    string a = "3333311111111111";
-    string b = "44422222221111";
+    string a = "99";
+    string b = "1";
 
     //CARRY: 0 1 1 0
     //       4 2 3 1
